46c1975ddb5be3ed2deda3e4e8cd93867d18caf2
rohitupadhhyay <upadhyay8486@gmail.com>
2024-09-09 10:54:39 +0530

    initial commit

---
+++ scripts.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const logoutButton = document.getElementById("logout");
+
+  logoutButton.addEventListener("click", () => {
+    alert("Logging out...");
+    // Implement actual logout logic here
+  });
+});
